21367751749be892b547d99a0d81fb0d633df844
Tobias Henn <tobias.henn@gmail.com>
2013-02-03 14:57:43 +0100

    adds 1st markdown trial

--- blog.rb
+++ blog.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
-require 'data_mapper'
 require 'haml'
-require 'bcrypt'
-require 'date'
-require 'sinatra/flash'
 require 'rdiscount'
 
-use Rack::Session::Cookie, :secret => 'FUCKITYOUCANTKNOWIT'
-enable :sessions
+# for all markdown files, use post.haml as layout
+set :markdown, :layout_engine => :haml, :layout => :layout
 
 
 get '/' do 
   haml :index
+end
+
+get '/blog' do 
+  markdown :test
 end
