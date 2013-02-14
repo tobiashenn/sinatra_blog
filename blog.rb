c96df664906192f06a93d4a41ff60cc08c71cfd3
Tobias Henn <tobias.henn@gmail.com>
2013-02-14 12:29:11 +0100

    first draft of auto-blog article list generation

--- blog.rb
+++ blog.rb
@@ -1,6 +1,8 @@
+# encoding: utf-8
 require 'sinatra'
 require 'haml'
 require 'rdiscount'
+require 'nokogiri'
 
 # for all markdown files, use post.haml as layout
 set :markdown, :layout_engine => :haml, :layout => :layout
@@ -10,6 +12,17 @@ get '/' do
   haml :index
 end
 
-get '/blog' do 
+get '/test' do 
   markdown :test
+end
+
+get '/content/:id' do
+  @content = RDiscount.new( File.open("content/" + params["id"].concat(".md")).read.force_encoding('UTF-8') ).to_html
+  haml :article_view
+end
+
+get '/blog' do
+  @content = Dir['content/*.md'].reverse
+  @articles = @content.map { |f| f.split('/').reverse.first}
+  haml :articles
 end
