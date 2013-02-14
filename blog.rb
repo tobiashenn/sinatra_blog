43fce3b3f1c16b10aa6505e66f151e2640ae5cd0
Tobias Henn <tobias.henn@gmail.com>
2013-02-14 17:50:40 +0100

    hopefully sorts articles correctly....

--- blog.rb
+++ blog.rb
@@ -17,12 +17,13 @@ get '/contact' do
 end
 
 get '/content/:id' do
+  @created_at = params["id"]
   @content = RDiscount.new( File.open("content/" + params["id"].concat(".md")).read.force_encoding('UTF-8') ).to_html
   haml :article_view
 end
 
 get '/blog' do
-  @dir = Dir['content/*.md'].reverse
+  @dir = Dir['content/*.md'].sort.reverse
   @articles = @dir.map { |c| c.split('/').reverse.first}
   @titles = Array.new
   haml :articles
